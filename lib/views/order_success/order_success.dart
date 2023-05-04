5b94e1f1901d33d300768087334eb695bd9520d0
Valk Dokk <91455763+TheValkDokk@users.noreply.github.com>
2023-05-05 05:50:30 +0700

    arrange buttons

--- lib/views/order_success/order_success.dart
+++ lib/views/order_success/order_success.dart
@@ -52,11 +52,13 @@ class OrderSuccessScreen extends StatelessWidget {
               width: Get.width * .7,
               height: Get.height * .05,
               child: FilledButton(
-                onPressed: () => Get.offNamedUntil(
-                  '/order_detail',
-                  ModalRoute.withName('/navhub'),
-                  arguments: id,
-                ),
+                onPressed: () {
+                  Get.offNamedUntil(
+                    '/order_detail',
+                    ModalRoute.withName('/navhub'),
+                    arguments: id,
+                  );
+                },
                 child: const Text("Xem đơn hàng"),
               ),
             ),
